fdf83edafaaf200c7366c1bfc763d8f682fd9cf2
lexscs <lex_scs@yahoo.com.br>
2023-09-01 10:49:38 -0300

    Aula 13
    Aula 13, orientação a objetos

---
+++ Aula13_Orientacao_Objetos/src/Principal.java
@@ -0,0 +1,18 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Pessoa pes = new Pessoa();
+		
+		pes.setNomePessoa("AlxWtnb");
+		pes.setIdadePessoa(45);
+		
+		System.out.println(pes.toString());
+		
+		
+		
+
+	}
+
+}
